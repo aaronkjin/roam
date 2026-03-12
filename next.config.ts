b8b6d00a9cbdcef0f4838dc33f6a5e4289b62901
aaronkjin <aaronkjin@gmail.com>
2026-03-12 02:31:28 -0700

    try proxy

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/clerk/:path*",
+        destination: "https://frontend-api.clerk.services/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
